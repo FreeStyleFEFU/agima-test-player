6ab75ba544799ca678a7ba8bc072280df4491f22
Gusev Mikhail <gsv@feip.co>
2023-10-18 08:40:25 +1000

    Fix paths to public data

--- src/mock/music.ts
+++ src/mock/music.ts
@@ -3,17 +3,17 @@ import { MusicItem } from "../types/music";
 export const mockMusicItems: MusicItem[] = [
   {
     title: "Linkin Park - Lost",
-    audioSrc: "/music/audio/Linkin Park-Lost.mp3",
-    coverSrc: "/music/covers/Linkin Park-Lost.jpg",
+    audioSrc: "/agima-test-player/music/audio/Linkin Park-Lost.mp3",
+    coverSrc: "/agima-test-player/music/covers/Linkin Park-Lost.jpg",
   },
   {
     title: "Grey Daze - Soul Song",
-    audioSrc: "/music/audio/Grey Daze-Soul Song.mp3",
-    coverSrc: "/music/covers/Grey Daze-Soul Song.jpg",
+    audioSrc: "/agima-test-player/music/audio/Grey Daze-Soul Song.mp3",
+    coverSrc: "/agima-test-player/music/covers/Grey Daze-Soul Song.jpg",
   },
   {
     title: "Grey Daze - Believe Me",
-    audioSrc: "/music/audio/Grey Daze-Believe Me.mp3",
-    coverSrc: "/music/covers/Grey Daze-Believe Me.jpg",
+    audioSrc: "/agima-test-player/music/audio/Grey Daze-Believe Me.mp3",
+    coverSrc: "/agima-test-player/music/covers/Grey Daze-Believe Me.jpg",
   },
 ];
